491d364cb6298caa28d7d5f5a9cc5d2df6109f3c
propertiess <erofeev2001@yandex.ru>
2023-03-20 14:39:54 +0300

    fix: on selector change remove request & add labels

--- src/screens/home/components/BookSelectors.tsx
+++ src/screens/home/components/BookSelectors.tsx
@@ -1,36 +1,23 @@
-import { useEffect } from 'react';
 import { Group, Select } from '@mantine/core';
 import { observer } from 'mobx-react-lite';
 
 import { useBookSearch } from '@/store/book-search';
-import { useBooks } from '@/store/books';
 import { CategoryEnum, SortEnum } from '@/types';
 import { CATEGORIES, SORTS } from '@/utils/consts';
 
 export const BookSelectors = observer(() => {
-  const { category, orderBy, setCategory, setOrderBy, query } = useBookSearch();
-  const { searchBooks } = useBooks();
-
-  useEffect(() => {
-    if (!query) {
-      return;
-    }
-
-    searchBooks({
-      query,
-      category,
-      orderBy
-    });
-  }, [category, orderBy, query, searchBooks]);
+  const { category, orderBy, setCategory, setOrderBy } = useBookSearch();
 
   return (
     <Group mt='md'>
       <Select
+        label='Categories'
         data={CATEGORIES}
         value={category}
         onChange={val => setCategory(val as CategoryEnum)}
       />
       <Select
+        label='Sorting by'
         data={SORTS}
         value={orderBy}
         onChange={val => setOrderBy(val as SortEnum)}
